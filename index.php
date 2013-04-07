c322cfe26297f087e1a4a843daf3bc5ec1a719a3
Björn Schotte <bjoern.schotte@googlemail.com>
2013-04-07 15:05:01 +0200

    changed class name

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * test file for auto-creation of Apfoo package class
+ *
+ * (c) 2013 Björn Schotte <bjoern.schotte@googlemail.com>
+ */
+require_once 'vendor/autoload.php';
+
+$foo = new Apfoo\Apfoo();
